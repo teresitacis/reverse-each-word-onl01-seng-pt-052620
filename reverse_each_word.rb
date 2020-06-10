01228220d4b44d7d5e0234b4a7df783acd166cb9
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 05:15:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
-words_array = sentence.split("")
+words_array = sentence.split(" ")
 new_array = []
 new_array = words_array.collect {|word| word.reverse }
-new_array.join("")
+new_array.join(" ")
 end
 
